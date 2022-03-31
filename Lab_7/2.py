802a550341913df872a1821f8ebbb1ffbdea594a
Chifwin <o.s.merezhko@gmail.com>
2022-04-01 01:17:40 +0600

    last

--- Lab_7/2.py
+++ Lab_7/2.py
@@ -1,12 +1,12 @@
 import pygame, os
 
 
-pygame.init()
 
 print("""Settings:
-Play/stop:      space
+Play/pause:     space
 Next:           right arrow
-Previous:       left arrow""")
+Previous:       left arrow
+Stop/restart:   escape""")
 
 FORMATS = ["flac", 'mp3', 'ogg', 'm4a']
 
@@ -16,19 +16,58 @@ while True:
         songs = [x for x in os.listdir(directory) if not os.path.isdir(rf"{directory}\{x}") and x.split('.')[-1] in FORMATS]
     except FileNotFoundError:
         print(f"{directory} doesn't exist")
-    else:
-        break
+    else: 
+        if len(songs) > 0:
+            break
+        else:
+            print("There aren't supported files")
 
 cur_song = 0
 
+pygame.init()
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((200, 200))
 done = False
+is_played = False
+
+def play():
+    pygame.mixer.music.unload()
+    print(f"Now playing {songs[cur_song]}")
+    pygame.mixer.music.load(rf"{directory}\{songs[cur_song]}")
+    pygame.mixer.music.play(-1)
+    is_played = True
+
+play()
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_LEFT:
+            cur_song -= 1
+            if (cur_song >= 0):
+                play()
+            else:
+                print("No previous song")
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_RIGHT:
+            cur_song += 1
+            if (cur_song < len(songs)):
+                play()
+            else:
+                print("No more songs")
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            if is_played:
+                pygame.mixer.music.stop()
+                is_played = False
+            else:
+                play()
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            if is_played:
+                pygame.mixer.music.pause()
+            else:
+                pygame.mixer.music.unpause()
+            is_played = not is_played
+
     clock.tick(60)
 
 

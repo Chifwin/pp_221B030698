01562b7669ba9eb40c6b4b29d1278657e4593f31
Chifwin <o.s.merezhko@gmail.com>
2022-04-01 00:29:02 +0600

    last nerve cell did this...

--- Lab_7/1.py
+++ Lab_7/1.py
@@ -1,14 +1,23 @@
+from matplotlib import scale
 import pygame
 from datetime import datetime
 
 
-def blit_rotate(screen, image, topleft, angle):
-    rotated_image = pygame.transform.rotate(image, angle)
-    #print(rotated_image.get_rect())
-    new_rect = rotated_image.get_rect(center = image.get_rect(bottomright = topleft).center)
-    #new_rect = rotated_image.get_rect(center = image.get_rect(bottomright = bottomright).center)
-    #print(new_rect)
-    screen.blit(rotated_image, new_rect)
+def rotate(screen, surface, angle, bias, radii):
+    """Rotate the surface around center of image displaysed by radii vector
+
+    Args:
+        surface (pygame.Surface): The surface that is to be rotated.
+        angle (float): Rotate by this angle.
+        bias (tuple, list, pygame.math.Vector2): Displaysement
+        radii (pygame.math.Vector2): Vector to pivot point from center of image
+    """
+    rotated_image = pygame.transform.rotate(surface, -angle)  # Rotate the image.
+    rotated_radii = radii.rotate(angle)  # Rotate the radii vector.
+    # Add the radii vector to the bias point to shift the rect.
+    rect = rotated_image.get_rect(center=bias+rotated_radii)
+    screen.blit(rotated_image, rect)
+
 
 pygame.init()
 screen_size = width, height = (500, 500)
@@ -16,27 +25,28 @@ screen = pygame.display.set_mode(screen_size)
 clock = pygame.time.Clock()
 done = False
 
-mickey_body = pygame.transform.scale(
-    pygame.image.load('Lab_7\\images\\Mickey_body.png'), screen_size)
-mickey_left_hand = pygame.transform.rotate(
-    pygame.image.load('Lab_7\\images\\Mickey_left_hand.png'), 60)
-mickey_right_hand = pygame.transform.rotate(
-    pygame.image.load('Lab_7\\images\\Mickey_right_hand.png'), -45)
+mickey_body = pygame.image.load('Lab_7\\images\\Mickey_body.png')
+mickey_left_hand = pygame.image.load('Lab_7\\images\\Mickey_left_hand.png')
+mickey_right_hand = pygame.image.load('Lab_7\\images\\Mickey_right_hand.png')
 
 start = datetime.now()
-mickey_left_hand = pygame.transform.rotate(mickey_left_hand, start.minute*6)
-mickey_right_hand = pygame.transform.rotate(mickey_right_hand, start.second*6)
 
-print(mickey_left_hand.get_rect())
+MICKEY_LEFT_HAND_OFFSET = pygame.math.Vector2(0, -73)
+MICKEY_RIGHT_HAND_OFFSET = pygame.math.Vector2(0, -65)
+BIAS = (250, 250)
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
     
-    screen.fill((255, 255, 255))
+    screen.fill((255, 0, 255))
     screen.blit(mickey_body, (0, 0))
 
-    blit_rotate(screen, mickey_right_hand, (0, 0), datetime.now().second*6)
+    rotate(screen, mickey_right_hand, datetime.now().second*6, BIAS, MICKEY_RIGHT_HAND_OFFSET)
+    rotate(screen, mickey_left_hand, datetime.now().minute*6, BIAS, MICKEY_LEFT_HAND_OFFSET)
+    pygame.display.set_caption(f'Minutes: {datetime.now().minute}, seconds: {datetime.now().second}')
     pygame.display.flip()
-    clock.tick(60)
+    clock.tick(1)
+
+pygame.quit()
---
+++ Lab_7/2.py
@@ -0,0 +1,4 @@
+import pygame, os
+
+
+directory = r"C:\Users\Oleg\Music\初音ミクシンフォニー～Miku Symphony 2020 オーケストラライブ (24-96)" #input("From which folder you want to listen music? ")
